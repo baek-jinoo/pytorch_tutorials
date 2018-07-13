0f63632f1dafc321867572b0b3a9503bb4fa16a4
Jinwoo Baek <baek32jinoo@gmail.com>
2018-07-13 10:23:26 -0700

    Implement different activation functions

--- my_relu.py
+++ my_relu.py
@@ -13,6 +13,48 @@ class MyReLU(torch.autograd.Function):
         grad_input[input < 0] = 0
         return grad_input
 
+def sigmoid(x):
+    """
+    A numerically stable version of the logistic sigmoid function.
+    """
+    pos_mask = (x >= 0)
+    neg_mask = (x < 0)
+    z = torch.zeros_like(x)
+    z[pos_mask] = torch.exp(-x[pos_mask])
+    z[neg_mask] = torch.exp(x[neg_mask])
+    top = torch.ones_like(x)
+    top[neg_mask] = z[neg_mask]
+    return top / (1 + z)
+
+class MySigmoid(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input):
+        return sigmoid(input)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad_input = grad_output.clone()
+        return (1 - sigmoid(grad_input)) * sigmoid(grad_input)
+
+class MyNewActivation(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input):
+        sigmoid_output = sigmoid(input)
+        ctx.save_for_backward(sigmoid_output, input)
+        return sigmoid_output * input
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        sigmoid_output, input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+
+        grad_input_sigmoid = input * grad_input
+        sigmoid_grad = (1 - sigmoid(grad_input_sigmoid)) * sigmoid(grad_input_sigmoid)
+
+        input_grad = sigmoid_output * grad_input
+
+        return sigmoid_grad + input_grad
+
 
 dtype = torch.float
 device = torch.device("cpu")
@@ -29,8 +71,13 @@ learning_rate = 1e-6
 
 for t in range(500):
     relu = MyReLU.apply
-
     y_pred = relu(x.mm(w1)).mm(w2)
+
+    #my_sigmoid = MySigmoid.apply
+    #y_pred = my_sigmoid(x.mm(w1)).mm(w2)
+
+    #my_new_activation = MyNewActivation.apply
+    #y_pred = my_new_activation(x.mm(w1)).mm(w2)
 
     loss = (y_pred - y).pow(2).sum()
     print(t, loss.item())
